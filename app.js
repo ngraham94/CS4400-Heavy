23b536741b913aeac1a83fbc5346a1d2cbd7bff6
Nickolas Graham <n.graham94@gmail.com>
2017-04-20 16:39:33 -0400

    Jquery added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function register() {
+$('#register').click(function() {
     alert("You clicked Register!");
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'register_form.html'),
         protocol: 'file:',
         slashes: true}));
-}
+})
 
-function login() {
-    alert("You clicked Register!");
-}
+$('#login').click(function() {
+    alert("You logged in yo!");
+})
--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const electron = require('electron')
 const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
-BrowserWindow.$ = BrowserWindow.jQuery = require('jquery');
 const path = require('path')
 const url = require('url')
 
